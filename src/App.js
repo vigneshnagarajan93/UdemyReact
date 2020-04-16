8abd79b71d5bf0a2d63e9f3ad44ea95554c9fb4f
Vignesh Nagarajan <vigneshviccky@gmail.com>
2020-04-16 16:15:16 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
+import React,{Component} from 'react';
+//import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
+import Person from './Person/Person.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  state={
+    persons:[
+      {name:"Test 1",age:"12"},
+      {name:"Test 2",age:"34"}
+    ]
+  }
+  switchHandler = (newName) => {
+    this.setState({
+        persons:[
+          {name:newName,age:"12"},
+          {name:"Test 2",age:"34"},
+        ]
+    })
+  }
+  nameChangedHandler = (event)=>{
+    this.setState({
+      persons:[
+        {name:event.target.value,age:"12"},
+        {name:"Test 2",age:"34"},
+      ]
+  })
+  }
+  render(){
+    return(
+      <div>
+        <h1>Xample</h1>
+        <button onClick={this.switchHandler.bind(this,'New name')}>Switch names</button>
+        <Person name={this.state.persons[0].name} changeHandlerRef={this.nameChangedHandler}click={this.switchHandler} age={this.state.persons[0].age}></Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
+      </div>
+    );
+  }
+}
+
+
+export default App;
+/*
+const App = props =>{
+  const [personsState,setPersonsState]=useState({
+    persons:[
+      {name:"Test 1",age:"12"},
+      {name:"Test 2",age:"34"}
+    ]
+  }
+  );
+  return(
+    <div>
+      <h1>Xample</h1>
+      
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}></Person>
+      <Person name={personsState.persons[1].name} click= age={personsState.persons[1].age}></Person>
     </div>
   );
 }
 
-export default App;
+export default App;*/
---
+++ src/Person/Person.js
@@ -0,0 +1,23 @@
+/*import React from 'react';
+
+
+const person = (props) => {
+return <p> I am {props.name} and my age is {props.age}</p>
+}
+
+export default person;*/
+import React, {Component} from 'react';
+
+class Person extends Component{
+    render(){
+        return(
+            <div>
+                <p onClick={this.props.click}>I am {this.props.name} and my age is {this.props.age}</p>
+                <p>{this.props.children} </p>
+                <input type="text" id="props" onChange={this.props.changeHandlerRef} name={this.props.name}/>
+            </div>
+        );
+    }
+}
+
+export default Person;
